243db6b9cc6b4def75eb31a633b919566be68a86
Jason <jbeals12@gmail.com>
2019-05-10 08:52:31 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,19 +1,8 @@
 object.addEventListener("load", myScript);
-<script>
-  object.onload = function(){myScript};
-      function openColorBox(){
-        $.colorbox({iframe:true, width:"80%", height:"80%", href: "http://www.sitepoint.com"});
-      }
-      
-      function countDown(){
-        seconds--
-        $("#seconds").text(seconds);
-        if (seconds === 0){
-          openColorBox();
-          clearInterval(i);
-        }
-      }
 
-      var seconds = 5,
-          i = setInterval(countDown, 1000);
-    </script>
+  object.onload = function(){myScript};
+   <script>
+function loadImage() {
+  alert("Image is loaded");
+}
+</script>
